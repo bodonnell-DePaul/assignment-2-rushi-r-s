08ca27e39dae370db943b57ca41cb61bd0cb5292
Rushikesh Suryawanshi <suryawanshirushikesh800@gmail.com>
2024-10-09 05:27:34 -0500

    cleared unwanted todoitems

--- todolist/src/todoItems.js
+++ todolist/src/todoItems.js
@@ -21,35 +21,6 @@
     Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. 
     Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. `, dueDate: '2024-04-11' },
-
-
-
-    
-    {
-      title: 'Todo 5', description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-      aliquip ex ea commodo consequat. Duis aute irure dolor in
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-      culpa qui officia deserunt mollit anim id est laborum.`, dueDate: '2024-10-17' },
-  
-      { title: 'Todo 6', description: `Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-      Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. 
-      Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis.`, dueDate: '2024-10-15' },
-  
-      { title: 'Todo 7', description: `Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-      Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. Vivamus nec nisi nec nunc mattis molestie. 
-      Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. Pellentesque habitant morbi tristique 
-      senectus et netus et malesuada fames ac turpis egestas.`, dueDate: '2024-10-12' },
-  
-      { title: 'Todo 8', description: `Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. 
-      Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. 
-      Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. `, dueDate: '2024-10-10' },
-
-
   ];
 
   export default todos;
